e65cb91a689c0235378abd9458ceda5f7a800d2a
Issufi Badji <45535344+issufibadji@users.noreply.github.com>
2023-01-09 17:15:39 -0300

    Add files via upload

---
+++ curso-gratuito-vue3-aula-20/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="updateUser()">
+        Atualizar perfil
+    </button>
+  <HelloWorld
+      user=""
+      msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+    data() {
+      return {
+      }
+    },
+
+    methods: {
+        updateUser() {
+            const newUser = {
+                first_name: 'Tiago',
+                last_name: 'Matos',
+                email: 'tiago@tiago.com'
+            }
+            this.$store.commit('storeUser', newUser)
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
